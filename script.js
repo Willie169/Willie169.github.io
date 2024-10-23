6683a8180170acb2d8c1f0276d8684bbf658d2dd
Willie169 <cg10250207@gmail.com>
2024-10-24 07:53:31 +0800

    p

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 const backToTopBtn = document.getElementById("backToTopBtn");
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-};
-
 function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
