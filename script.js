292c89ae8fa877a496385468a84faf420f450717
Willie169 <cg10250207@gmail.com>
2024-10-24 19:23:39 +0800

    l

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function scrollToTop() {
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, c - c / 8);
-  }
+  document.getElementById('h1').scrollIntoView();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tocList = document.getElementById('toc-list');
+  const headers = document.querySelectorAll('h2, h3');
+
+  headers.forEach((header, index) => {
+    const id = `header-${index}`;
+    header.id = id;
+
+    const listItem = document.createElement('li');
+    listItem.classList.add(header.tagName.toLowerCase());
+    listItem.innerHTML = `<a href="#${id}">${header.textContent}</a>`;
+    tocList.appendChild(listItem);
+  });
+});
