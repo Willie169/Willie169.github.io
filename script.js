47f8016eeb6f7151e11debe51204cda46752fbbc
Willie169 <cg10250207@gmail.com>
2024-10-24 08:05:16 +0800

    a

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const backToTopBtn = document.getElementById("backToTopBtn");
 
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 }
