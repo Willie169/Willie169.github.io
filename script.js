86526aa929d4c2ff6be8154a24e74448a3b7a799
Willie169 <cg10250207@gmail.com>
2024-10-24 16:07:13 +0800

    b

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let backToTopBtn = document.getElementById("backToTopBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-}
-
 function scrollToTop() {
   const c = document.documentElement.scrollTop || document.body.scrollTop;
   if (c > 0) {
