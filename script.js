2b445e80e1f032c7a3a0cd731dd800c16bc7a399
Willie169 <cg10250207@gmail.com>
2024-10-24 15:23:39 +0800

    y

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
 function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
 }
