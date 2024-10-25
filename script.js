0782d6e6a18df7cc6d07834208ba10969fe14710
Willie169 <cg10250207@gmail.com>
2024-10-25 10:00:04 +0800

    l

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const tocList = document.getElementById('toc-list');
   const headers = document.querySelectorAll('h2, h3');
 
-  headers.forEach((header, index) => {
-    const id = `hd${index}`;
-    header.id = id;
+  headers.forEach((header) => {
+    if (!header.id) {
+      header.id = header.textContent
+        .toLowerCase()
+        .replace(/\s+/g, '')
+        .replace(/[^\w\-]+/g, '')
+        .replace(/^-+|-+$/g, '');
+    }
 
     const listItem = document.createElement('li');
     listItem.classList.add(header.tagName.toLowerCase());
-    listItem.innerHTML = `<a href="#${id}">${header.textContent}</a>`;
+    listItem.innerHTML = `<a href="#${header.id}">${header.textContent}</a>`;
     tocList.appendChild(listItem);
   });
 });
