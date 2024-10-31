9bedcf28f185c841fd66dadb740e9a7d0102dd43
Willie169 <cg10250207@gmail.com>
2024-10-31 19:33:39 +0800

    5

--- script.js
+++ script.js
@@ -4,23 +4,3 @@ function scrollToTop() {
     block: 'start'
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  const tocList = document.getElementById('toc-list');
-  const headers = document.querySelectorAll('h2, h3');
-
-  headers.forEach((header) => {
-    if (!header.id) {
-      header.id = header.textContent
-        .toLowerCase()
-        .replace(/\s+/g, '')
-        .replace(/[^\w\-]+/g, '')
-        .replace(/^-+|-+$/g, '');
-    }
-
-    const listItem = document.createElement('li');
-    listItem.classList.add(header.tagName.toLowerCase());
-    listItem.innerHTML = `<a href="#${header.id}">${header.textContent}</a>`;
-    tocList.appendChild(listItem);
-  });
-});
